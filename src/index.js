b743a979e9e4053a1afd21590b093257828b79b1
yoaddb <yoadberkovich@walla.co.il>
2020-06-28 01:01:34 +0300

    extract prisma secret to env var

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import "@babel/polyfill";
+import "@babel/polyfill/noConflict";
 import { GraphQLServer, PubSub } from "graphql-yoga";
 import { resolvers, fragmentReplacements } from "./resolvers/index";
 import prisma from "./prisma";
